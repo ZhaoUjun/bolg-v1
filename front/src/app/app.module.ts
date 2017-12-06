a7357f632086b70d77f440d804aae522bd2ab71b
rujun <zhaorujunfor@tianzehuitong.com>
2017-12-06 23:34:04 +0800

    add router

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,15 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MianComponent } from './mian/mian.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MianComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
---
+++ front/src/app/mian/mian.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mian',
+  templateUrl: './mian.component.html',
+  styleUrls: ['./mian.component.css']
+})
+export class MianComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
