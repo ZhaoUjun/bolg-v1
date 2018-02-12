0b3a8aa407931a43ecb1baaf6bb14875bd0628d7
ujun <zhaorujunfor@tianzehuitong.com>
2018-02-12 15:52:47 +0800

    host update

--- admin/src/utils/appMiddlewares.js
+++ admin/src/utils/appMiddlewares.js
@@ -28,7 +28,7 @@ export function checkSignature(req, res, next) {
 
 export function allowCORS(req, res, next) {
     res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200,http://119.23.255.113:3030");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200,http://119.23.255.113");
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "X-Requested-With,Authorization,Sign,Time,Content-Type,APPId,AppKey");
     next()
